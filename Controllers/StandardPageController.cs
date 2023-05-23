bcbdd9c94db4f66d18a81578a31df010852e7941
ALI Max <max.ali@soprasteria.com>
2023-05-23 14:31:00 +0200

    First commit

---
+++ Controllers/StandardPageController.cs
@@ -0,0 +1,15 @@
+﻿using EPiServer.Web.Mvc;
+using max.Cms12.Models.Pages;
+using Microsoft.AspNetCore.Mvc;
+
+namespace max.Cms12.Reference.Commerce.Site.Features.Shared.Controllers
+{
+    public class StandardPageController : PageController<StandardPage>
+    {
+        [HttpGet]
+        public IActionResult Index(StandardPage currentPage)
+        {
+            return View(currentPage);
+        }
+    }
+}
